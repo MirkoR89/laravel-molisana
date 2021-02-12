59af331e005b975bbe42564ecdf0c7385be3af80
Mirko <mirko.rinelli@gmail.com>
2021-02-12 12:33:22 +0100

    Html and css header completed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,15 @@
 <header>
-   <div class="logo"><img src="{{ asset('img/logo.png') }}" alt=""></div> 
+   {{-- Logo --}}
+   <div class="logo d-flex justify-content-center">
+      <img src="{{ asset('img/logo.png') }}" alt="">
+   </div> 
+
+   {{-- Navbar --}}
+   <nav class="d-flex justify-content-center align-items-center">
+      <ul>
+         <li><a href="">Home</a></li>
+         <li><a href="">Prodotti</a></li>
+         <li><a href="">Contatti</a></li>
+      </ul>
+   </nav>
 </header>
