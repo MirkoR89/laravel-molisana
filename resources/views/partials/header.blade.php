a00e2d4b2664035e9c5006273309dfd555eaac73
Mirko <mirko.rinelli@gmail.com>
2021-02-12 12:49:50 +0100

    Layout pages fixed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,9 +7,9 @@
    {{-- Navbar --}}
    <nav class="d-flex justify-content-center align-items-center">
       <ul>
-         <li><a href="">Home</a></li>
-         <li><a href="">Prodotti</a></li>
-         <li><a href="">Contatti</a></li>
+         <li><a href="{{'/'}}">Home</a></li>
+         <li><a href="{{'products'}}">Prodotti</a></li>
+         <li><a href="{{'contacts'}}">Contatti</a></li>
       </ul>
    </nav>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {     
-    return view('welcome');
-});
-
-Route::get('home', function () {     
-    return view('layouts.home');
+    return view('home');
 });
 
 Route::get('products', function () {
